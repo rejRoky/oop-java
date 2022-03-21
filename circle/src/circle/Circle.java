9f9bc4ad42d74d01562e833755bcf7efca72a0c8
rejRoky <rejaul.islam.roky@gmail.com>
2022-03-22 05:50:33 +0600

    circle folder added

---
+++ circle/src/circle/Circle.java
@@ -0,0 +1,22 @@
+
+package circle;
+
+public class Circle {
+
+
+    public static void main(String[] args) {
+        System.out.println("hello, OOP of Java");
+        System.out.println("hello, OOP of Java");
+        System.out.println("hello, OOP of Java");
+        
+        System.out.println("hello, OOP of Java");
+        
+        System.out.println();
+        
+        Rectangle r ;
+        
+        
+       
+    }
+    
+}
