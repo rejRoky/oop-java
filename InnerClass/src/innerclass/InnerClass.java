db3999181ae60629372ed282226e0d2fd67fe1a7
rejRoky <rejaul.islam.roky@gmail.com>
2022-04-01 03:51:35 +0600

    InnerClass updated : local inner class and anonimus class

--- InnerClass/src/innerclass/InnerClass.java
+++ InnerClass/src/innerclass/InnerClass.java
@@ -1,5 +1,11 @@
 package innerclass;
 
+///Anonimus class
+interface My {
+
+    void show();
+}
+
 /// Nested Inner Class
 class Outer {
 
@@ -21,6 +27,36 @@ class Outer {
         System.out.println(i.y);
 
     }
+
+    ////
+    public void Display() {
+
+        ///Local Inner class
+        class Inner {
+
+            public void Show() {
+                System.out.println("Local Inner");
+            }
+
+        }
+        Inner i = new Inner();
+        i.Show();
+
+        new Inner().Show(); // Anonimus Object// this obj only use for this line 
+
+        ////
+    }
+
+    ///Anonimus class
+    public void display() {
+        new My() {
+            @Override
+            public void show() {
+                System.out.println("Annonimus Inner class");
+            }
+        }.show(); /// Anonimus Object// this obj only use for this line
+
+    }
 }
 
 public class InnerClass {
@@ -31,8 +67,15 @@ public class InnerClass {
         Outer o = new Outer();
         o.outerDisplay();
 
+        /// Nested innder class  : direetly access
         Outer.Inner oi = new Outer().new Inner();
         oi.innerDisplay();
+
+        ///Local Inner class
+        o.Display();
+
+        ///Anonimus class
+        o.display();
 
     }
 
