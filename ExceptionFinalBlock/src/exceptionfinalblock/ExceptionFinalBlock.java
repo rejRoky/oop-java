ea2d4ec5e221076087a18d7c44ee678115b0cd62
rejRoky <rejaul.islam.roky@gmail.com>
2022-04-02 03:39:38 +0600

    ExceptionFinalBlock uploaded

---
+++ ExceptionFinalBlock/src/exceptionfinalblock/ExceptionFinalBlock.java
@@ -0,0 +1,25 @@
+package exceptionfinalblock;
+
+public class ExceptionFinalBlock {
+
+    static void meth1() throws Exception {
+        try {
+
+            throw new Exception();
+        } finally {
+            //this message will definitly execute 
+            System.out.println("Final Message");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        try {
+            meth1();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+
+}
