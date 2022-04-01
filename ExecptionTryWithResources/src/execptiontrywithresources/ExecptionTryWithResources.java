99335095a49f894d0382981b50354fa36eecc613
rejRoky <rejaul.islam.roky@gmail.com>
2022-04-02 03:43:05 +0600

    ExecptionTryWithResources uploaded

---
+++ ExecptionTryWithResources/src/execptiontrywithresources/ExecptionTryWithResources.java
@@ -0,0 +1,30 @@
+package execptiontrywithresources;
+
+import java.io.*;
+import java.util.*;
+
+public class ExecptionTryWithResources {
+
+    static void Divide() throws Exception {
+
+        try ( FileInputStream fi = new FileInputStream("Test.txt");  Scanner sc = new Scanner(fi)) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            System.out.println(a / c);
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        try {
+            Divide();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        //int x=sc.nextInt();
+        // System.out.println(x);
+    }
+
+}
