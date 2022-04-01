c2dcebb39bb450fbe5054f2e0f988907b984112f
rejRoky <rejaul.islam.roky@gmail.com>
2022-04-02 01:33:59 +0600

    ExceptionThrowThrows Updated : own Exception class added

--- ExceptionThrowThrows/src/exceptionthrowthrows/ExceptionThrowThrows.java
+++ ExceptionThrowThrows/src/exceptionthrowthrows/ExceptionThrowThrows.java
@@ -1,32 +1,41 @@
 package exceptionthrowthrows;
 
+class NegativeDimensionException extends Exception {
+
+    @Override
+    public String toString() {
+        return "Dimension of Reatangle cannot be Negative";
+
+    }
+}
+
 public class ExceptionThrowThrows {
 
-    static int area(int a, int b) throws Exception {
+    static int area(int a, int b) throws NegativeDimensionException {
         if (a < 0 || b < 0) {
-            throw new Exception();
+            throw new NegativeDimensionException();
         }
 
         return a * b;
 
     }
 
-    static void meth1() throws Exception {
+    static void meth1() throws NegativeDimensionException {
         System.out.println(area(-10, 6));
     }
 
-    static void meth2() throws Exception {
+    static void meth2() throws NegativeDimensionException {
         meth1();
     }
 
-    static void meth3() throws Exception {
+    static void meth3() throws NegativeDimensionException {
         meth2();
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws NegativeDimensionException {
         try {
             meth3();
-        } catch (Exception e) {
+        } catch (NegativeDimensionException e) {
             System.out.println(e);
         }
     }
